0536136e9b4e23d00f768879c94efd104010d52a
buingockhanh0306 <buikhanhi2k@gmail.com>
2022-05-25 09:56:00 +0700

    add Javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 const link = document.querySelectorAll(".link-no-active");
 for (var i = 0; i < link.length; i++) {
-  link[i].addEventListener("click", function () {
-    this.classList.add("active");
-  });
+  for (let j = 0; j < link.length; j++) {
+    link[i].addEventListener("click", function () {
+      this.classList.add("active");
+      if (j !== i) {
+        link[j].classList.remove("active");
+      }
+    });
+  }
 }
